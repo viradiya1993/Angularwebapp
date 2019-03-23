b6559d8f27054a1756357526d13c8742ee6424ed
MOONTECH\kalpesh <kalpesh>
2019-03-23 14:12:47 +0530

    matter details add done

--- src/app/main/pages/matters/matters-detail/matters-detail.component.ts
+++ src/app/main/pages/matters/matters-detail/matters-detail.component.ts
@@ -1,19 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+
+import { FormBuilder, FormGroup } from '@angular/forms';
+
 import { ActivatedRoute } from '@angular/router';
+import { fuseAnimations } from '@fuse/animations';
+
+import { MattersService } from '../matters.service';
 
 @Component({
   selector: 'app-matters-detail',
   templateUrl: './matters-detail.component.html',
-  styleUrls: ['./matters-detail.component.scss']
+  styleUrls: ['./matters-detail.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  animations: fuseAnimations
 })
 export class MattersDetailComponent implements OnInit {
-
+  currentMatterId: any;
+  currentMatter: any;
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private _mattersService: MattersService,
+    private _formBuilder: FormBuilder
   ) { }
 
   ngOnInit() {
-    this.route.data.subscribe(v => console.log(v));
+    this.route.url.subscribe(v =>
+      this.currentMatterId = v[0].path
+    );
+    this.currentMatter = {
+      'id': '5725a680b3249760ea21de52',
+      'name': 'Abbott',
+      'lastName': 'Keitch',
+      'avatar': 'assets/images/avatars/Abbott.jpg',
+      'nickname': 'Royalguard',
+      'company': 'Saois',
+      'jobTitle': 'Digital Archivist',
+      'email': 'abbott@withinpixels.com',
+      'phone': '+1-202-555-0175',
+      'address': '933 8th Street Stamford, CT 06902',
+      'birthday': '',
+      'notes': ''
+    };
   }
 
 }
--- src/app/main/pages/matters/matters.module.ts
+++ src/app/main/pages/matters/matters.module.ts
@@ -8,7 +8,7 @@ import { AuthGuard } from '../../../_guards';
 import { FuseConfirmDialogModule, FuseSidebarModule } from '@fuse/components';
 
 import { MattersComponent } from './matters.component';
-import { MatButtonModule, MatCheckboxModule, MatSlideToggleModule, MatCardModule, MatSelectModule, MatDatepickerModule, MatFormFieldModule, MatIconModule, MatInputModule, MatMenuModule, MatRippleModule, MatTableModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatTabsModule, MatExpansionModule, MatSlideToggleModule, MatCardModule, MatSelectModule, MatDatepickerModule, MatFormFieldModule, MatIconModule, MatInputModule, MatMenuModule, MatRippleModule, MatTableModule, MatToolbarModule } from '@angular/material';
 import { MattersListComponent } from './matters-list/matters-list.component';
 import { MattersSortDetailComponent } from './matters-sort-detail/matters-sort-detail.component';
 import { MattersDetailComponent } from './matters-detail/matters-detail.component';
@@ -44,7 +44,9 @@ const routes = [
     MatToolbarModule,
     MatSelectModule,
     MatCardModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatExpansionModule,
+    MatTabsModule
   ],
   exports: [
     MattersComponent

1c66b24cbb94dc46dbc86cc4c7b2d382db2b8763
MOONTECH\kalpesh <kalpesh>
2019-03-25 19:55:42 +0530

    legal detail popup done

--- src/app/main/pages/legal-details/authorities/authorities.component.ts
+++ src/app/main/pages/legal-details/authorities/authorities.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
-import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { MatPaginator, MatTableDataSource, MatDialog, MatDialogConfig } from '@angular/material';
 import { fuseAnimations } from '@fuse/animations';
+import { SortingDialogComponent } from 'app/main/sorting-dialog/sorting-dialog.component';
 
 @Component({
   selector: 'app-authorities',
@@ -14,10 +15,29 @@ export class AuthoritiesComponent implements OnInit {
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
     this.dataSource.paginator = this.paginator;
+  }
+  openDialog() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '50%';
+    dialogConfig.data = { 'data': ['topic', 'authority', 'citation', 'reference', 'web_address', 'comment'], 'type': 'authorities' };
+    //open pop-up
+    const dialogRef = this.dialog.open(SortingDialogComponent, dialogConfig);
+    //Save button click
+    // dialogRef.afterClosed().subscribe(result => {
+    //   console.log(result);
+    // });
+    dialogRef.afterClosed().subscribe(data =>
+      this.tableSetting(data)
+    );
+  }
+  tableSetting(data: any) {
+    if (data !== false) {
+      this.displayedColumns = data;
+    }
   }
 
 }
@@ -31,10 +51,10 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {topic:1, authority: 'xyz', citation: 1.0079, reference:'www.google.com',web_address:'xyz',comment:'not done yet'},
-  {topic:2, authority: 'xyz', citation: 1.0079, reference:'www.google.com',web_address:'xyz',comment:'not done yet'},
-  {topic:3, authority: 'xyz', citation: 1.0079, reference:'www.google.com',web_address:'xyz',comment:'not done yet'},
-  {topic:4, authority: 'xyz', citation: 1.0079, reference:'www.google.com',web_address:'xyz',comment:'not done yet'},
-  {topic:5, authority: 'xyz', citation: 1.0079, reference:'www.google.com',web_address:'xyz',comment:'not done yet'},
-  {topic:6, authority: 'xyz', citation: 1.0079, reference:'www.google.com',web_address:'xyz',comment:'not done yet'},
+  { topic: 1, authority: 'xyz', citation: 1.0079, reference: 'www.google.com', web_address: 'xyz', comment: 'not done yet' },
+  { topic: 2, authority: 'xyz', citation: 1.0079, reference: 'www.google.com', web_address: 'xyz', comment: 'not done yet' },
+  { topic: 3, authority: 'xyz', citation: 1.0079, reference: 'www.google.com', web_address: 'xyz', comment: 'not done yet' },
+  { topic: 4, authority: 'xyz', citation: 1.0079, reference: 'www.google.com', web_address: 'xyz', comment: 'not done yet' },
+  { topic: 5, authority: 'xyz', citation: 1.0079, reference: 'www.google.com', web_address: 'xyz', comment: 'not done yet' },
+  { topic: 6, authority: 'xyz', citation: 1.0079, reference: 'www.google.com', web_address: 'xyz', comment: 'not done yet' },
 ];
--- src/app/main/pages/legal-details/file-notes/file-notes.component.ts
+++ src/app/main/pages/legal-details/file-notes/file-notes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
-import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { MatPaginator, MatTableDataSource, MatDialog, MatDialogConfig } from '@angular/material';
 import { fuseAnimations } from '@fuse/animations';
+import { SortingDialogComponent } from 'app/main/sorting-dialog/sorting-dialog.component';
 
 
 @Component({
@@ -15,10 +16,29 @@ export class FileNotesComponent implements OnInit {
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
- constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
-     this.dataSource.paginator = this.paginator;
+    this.dataSource.paginator = this.paginator;
+  }
+  openDialog() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '50%';
+    dialogConfig.data = { 'data': ['date', 'topic', 'reference', 'event_agreed', 'brief_page_no', 'comment', 'privileged', 'witnesses', 'text'], 'type': 'authorities' };
+    //open pop-up
+    const dialogRef = this.dialog.open(SortingDialogComponent, dialogConfig);
+    //Save button click
+    // dialogRef.afterClosed().subscribe(result => {
+    //   console.log(result);
+    // });
+    dialogRef.afterClosed().subscribe(data =>
+      this.tableSetting(data)
+    );
+  }
+  tableSetting(data: any) {
+    if (data !== false) {
+      this.displayedColumns = data;
+    }
   }
 }
 export interface PeriodicElement {
@@ -29,12 +49,12 @@ export interface PeriodicElement {
 
 const ELEMENT_DATA: PeriodicElement[] = [
 
-  {date:new Date('2/1/2014'),time:11.30,note:'work in progress '},
-  {date:new Date('2/12/2014'),time:15.30,note:'work in progress '},
-  {date:new Date('6/13/2014'),time:15.30,note:'work in progress '},
-  {date:new Date('5/13/2014'),time:14.56,note:' work in progress'},
-  {date:new Date('1/13/2014'),time:13.30,note:' work in progress'},
-  {date:new Date('1/11/2014'),time:23.30,note:'work in progress '},
-  
+  { date: new Date('2/1/2014'), time: 11.30, note: 'work in progress ' },
+  { date: new Date('2/12/2014'), time: 15.30, note: 'work in progress ' },
+  { date: new Date('6/13/2014'), time: 15.30, note: 'work in progress ' },
+  { date: new Date('5/13/2014'), time: 14.56, note: ' work in progress' },
+  { date: new Date('1/13/2014'), time: 13.30, note: ' work in progress' },
+  { date: new Date('1/11/2014'), time: 23.30, note: 'work in progress ' },
+
 ];
 

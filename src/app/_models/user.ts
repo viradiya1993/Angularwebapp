42b20e3f780f883ad355163da71ace5dcca8029c
MOONTECH\kalpesh <kalpesh>
2019-04-02 14:38:43 +0530

    login and permissins set

--- src/app/_models/user.ts
+++ src/app/_models/user.ts
@@ -1,8 +1,17 @@
 export class User {
-    id: number;
-    username: string;
-    password: string;
-    firstName: string;
-    lastName: string;
     token?: string;
+    SessionToken:string;
+    TimeStampUTC: string;
+    TimeStamp: string;
+    LoggedInStatus:string;
+    LicenceNumber:string;
+    AllowMobileAccess: string;
+    UserGuid: string;
+    UserId: string;
+    UserName:string;
+    ProductType: string;
+    DefaultQuantityType: string;
+    InitialWindow: string;
+    ShowOutstanding: string;
+    ShowWIPGraph:string;
 }
--- src/app/main/authentication/login/login.component.ts
+++ src/app/main/authentication/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { fuseAnimations } from '@fuse/animations';
 import { FuseConfigService } from '@fuse/services/config.service';
-import { AuthenticationService } from '../../../_services';
+import { AuthenticationService, AppPermissionsService } from '../../../_services';
 import { Router, ActivatedRoute } from '@angular/router';
 import { first } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
@@ -24,6 +24,7 @@ export class LoginComponent implements OnInit {
     private _fuseConfigService: FuseConfigService,
     private _formBuilder: FormBuilder,
     private authenticationService: AuthenticationService,
+    private _AppPermissionsService: AppPermissionsService,
     private route: ActivatedRoute,
     private router: Router,
     private http: HttpClient,
@@ -34,8 +35,7 @@ export class LoginComponent implements OnInit {
       layout: { navbar: { hidden: true }, toolbar: { hidden: true }, footer: { hidden: true }, sidepanel: { hidden: true } }
     };
   }
-  //For my Api
-  //Loginapi = "https://api.silq.com.au/login";
+
   ngOnInit() {
     this.loginForm = this._formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
@@ -48,14 +48,13 @@ export class LoginComponent implements OnInit {
     return this.loginForm.controls;
   }
   loginUser() {
-    this.authenticationService.login(this.f.email.value, this.f.password.value);
-    this.router.navigate(['matters']);
-    // this.authenticationService.login(this.f.email.value, this.f.password.value).pipe(first()).subscribe(data => {
-    //   if (data) {
-    //     this.router.navigate(['matters']);
-    //   }
-    // }, error => {
-    //   this.toastr.error(error);
-    // });
+    this.authenticationService.login(this.f.email.value, this.f.password.value).pipe(first()).subscribe(data => {
+      if (data) {
+        this._AppPermissionsService.applictionSetting(JSON.parse(localStorage.getItem('Login_response')));
+        this.router.navigate(['matters']);
+      }
+    }, error => {
+      this.toastr.error(error);
+    });
   }
 }

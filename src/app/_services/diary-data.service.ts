21da1cffe2f32c94a4b89608802d691052af5edb
MOONTECH\kalpesh <kalpesh>
2019-04-09 20:46:50 +0530

    diary service

--- src/app/_services/diary-data.service.ts
+++ src/app/_services/diary-data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +9,7 @@ export class DiaryDataService {
 
   constructor(private http: HttpClient) { }
 
-  DiaryData(){
+  DiaryData() {
     return this.http.get<any>(environment.APIEndpoint + 'GetAppointment');
   }
 }

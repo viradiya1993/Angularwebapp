4d80e1e1df4cb1fd5efde8a2868ef9ec02de3add
MOONTECH\kalpesh <kalpesh>
2019-04-12 16:03:53 +0530

    login api change

--- src/app/_services/app-permissions.service.ts
+++ src/app/_services/app-permissions.service.ts
@@ -6,41 +6,14 @@ export class AppPermissionsService {
 
   constructor() { }
   PermissionsCons = {
-    'Permissions - Matter Details': '1',
-    'Permissions - Day Book / Time Entries': '2',
-    'Permissions - Contacts': '3',
-    'Permissions - Estimates': '4',
-    'Permissions - Document/Email Generation': '5',
-    'Permissions - Document Register': '6',
-    'Permissions - Invoicing': '7',
-    'Permissions - Receive Money': '8',
-    'Permissions - Spend Money': '9',
-    'Permissions - Chronology': '10',
-    'Permissions - Topics': '11',
-    'Permissions - Authorities': '12',
-    'Permissions - File Notes': '13',
-    'Permissions - Safe Custody': '14',
-    'Permissions - Safe Custody Packet': '15',
-    'Permissions - Searching': '16',
-    'Permissions - Diary': '17',
-    'Permissions - Tasks': '18',
-    'Permissions - Chart of Accounts': '19',
-    'Permissions - General Journal': '20',
-    'Permissions - Other Accounting': '21',
-    'Permissions - Trust Money': '22',
-    'Permissions - Trust Chart of Accounts': '23',
-    'Permissions - Trust General Journal': '24',
-    'Permissions - Trust Reports': '25',
-    'Permissions - Accounting Reports': '26',
-    'Permissions - Management Reports': '27',
-    'Permissions - System': '28',
-    'Permissions - Users': '29',
-    'Permissions - Activities/Sundries': '30',
+    'MATTER DETAILS': '1', 'DAY BOOK / TIME ENTRIES': '2', 'CONTACTS': '3', 'ESTIMATES': '4', 'DOCUMENT/EMAIL GENERATION': '5', 'DOCUMENT REGISTER': '6', 'INVOICING': '7',
+    'RECEIVE MONEY': '8', 'SPEND MONEY': '9', 'CHRONOLOGY': '10', 'TOPICS': '11', 'AUTHORITIES': '12', 'FILE NOTES': '13', 'SAFE CUSTODY': '14', 'SAFE CUSTODY PACKET': '15',
+    'SEARCHING': '16', 'DIARY': '17', 'TASKS': '18', 'CHART OF ACCOUNTS': '19', 'GENERAL JOURNAL': '20', 'OTHER ACCOUNTING': '21', 'TRUST MONEY': '22', 'TRUST CHART OF ACCOUNTS': '23',
+    'TRUST GENERAL JOURNAL': '24', 'TRUST REPORTS': '25', 'ACCOUNTING REPORTS': '26', 'MANAGEMENT REPORTS': '27', 'SYSTEM': '28', 'USERS': '29', 'ACTIVITIES/SUNDRIES': '30',
   };
 
   Permissions = [];
   applictionSetting(loginResponse: any) {
-    console.log(loginResponse);
     if (loginResponse) {
       let LoginData = {
         SessionToken: loginResponse.SESSIONTOKEN,
@@ -62,42 +35,15 @@ export class AppPermissionsService {
       localStorage.setItem('session_token', loginResponse.SESSIONTOKEN);
       let theme_name = loginResponse.PRODUCTTYPE == "Barrister" ? "theme-default" : "theme-yellow-light";
       localStorage.setItem('theme_type', theme_name);
-      // this.appPermissions(loginResponse);
+      this.appPermissions(loginResponse.PERMISSIONS);
       localStorage.removeItem('Login_response');
     }
   }
   appPermissions(setting: any) {
     if (setting) {
-      this.setObj(setting['Permissions - Matter Details'], 'Permissions - Matter Details');
-      this.setObj(setting['Permissions - Day Book / Time Entries'], 'Permissions - Day Book / Time Entries');
-      this.setObj(setting['Permissions - Contacts'], 'Permissions - Contacts');
-      this.setObj(setting['Permissions - Estimates'], 'Permissions - Estimates');
-      this.setObj(setting['Permissions - Document/Email Generation'], 'Permissions - Document/Email Generation');
-      this.setObj(setting['Permissions - Document Register'], 'Permissions - Document Register');
-      this.setObj(setting['Permissions - Invoicing'], 'Permissions - Invoicing');
-      this.setObj(setting['Permissions - Receive Money'], 'Permissions - Receive Money');
-      this.setObj(setting['Permissions - Spend Money'], 'Permissions - Spend Money');
-      this.setObj(setting['Permissions - Chronology'], 'Permissions - Chronology');
-      this.setObj(setting['Permissions - Topics'], 'Permissions - Topics');
-      this.setObj(setting['Permissions - Authorities'], 'Permissions - Authorities');
-      this.setObj(setting['Permissions - File Notes'], 'Permissions - File Notes');
-      this.setObj(setting['Permissions - Safe Custody'], 'Permissions - Safe Custody');
-      this.setObj(setting['Permissions - Safe Custody Packet'], 'Permissions - Safe Custody Packet');
-      this.setObj(setting['Permissions - Searching'], 'Permissions - Searching');
-      this.setObj(setting['Permissions - Diary'], 'Permissions - Diary');
-      this.setObj(setting['Permissions - Tasks'], 'Permissions - Tasks');
-      this.setObj(setting['Permissions - Chart of Accounts'], 'Permissions - Chart of Accounts');
-      this.setObj(setting['Permissions - General Journal'], 'Permissions - General Journal');
-      this.setObj(setting['Permissions - Other Accounting'], 'Permissions - Other Accounting');
-      this.setObj(setting['Permissions - Trust Money'], 'Permissions - Trust Money');
-      this.setObj(setting['Permissions - Trust Chart of Accounts'], 'Permissions - Trust Chart of Accounts');
-      this.setObj(setting['Permissions - Trust General Journal'], 'Permissions - Trust General Journal');
-      this.setObj(setting['Permissions - Trust Reports'], 'Permissions - Trust Reports');
-      this.setObj(setting['Permissions - Accounting Reports'], 'Permissions - Accounting Reports');
-      this.setObj(setting['Permissions - Management Reports'], 'Permissions - Management Reports');
-      this.setObj(setting['Permissions - System'], 'Permissions - System');
-      this.setObj(setting['Permissions - Users'], 'Permissions - Users');
-      this.setObj(setting['Permissions - Activities/Sundries'], 'Permissions - Activities/Sundries');
+      for (let i in setting) {
+        this.setObj(setting[i], i);
+      }
       localStorage.setItem('app_permissions', JSON.stringify(this.Permissions));
     }
   }

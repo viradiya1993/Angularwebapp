28c4b013aac4949a933d885a3dabf7e50d287a94
MOONTECH\kalpesh <kalpesh>
2019-06-22 16:01:03 +0530

    filter remove in time and billling

--- src/app/main/pages/time-billing/work-in-progress/work-in-progress.component.ts
+++ src/app/main/pages/time-billing/work-in-progress/work-in-progress.component.ts
@@ -22,47 +22,29 @@ export class WorkInProgressComponent implements OnInit {
   pageSize: any;
   tempColobj: any;
   isShowDrop: boolean;
-  lastFilter:any
+  lastFilter: any
   @ViewChild(MatPaginator) paginator: MatPaginator;
   isLoadingResults: boolean = false;
-  constructor( public datepipe: DatePipe,private dialog: MatDialog, private fb: FormBuilder, private WorkInProgress: WorkInProgressService, private TableColumnsService: TableColumnsService, private toastr: ToastrService) {
+  constructor(public datepipe: DatePipe, private dialog: MatDialog, private fb: FormBuilder, private WorkInProgress: WorkInProgressService, private TableColumnsService: TableColumnsService, private toastr: ToastrService) {
     this.lastFilter = JSON.parse(localStorage.getItem('Work_in_progress_filter'));
     let currentUser = JSON.parse(localStorage.getItem('currentUser'));
     this.isShowDrop = currentUser.ProductType == "Barrister" ? false : true;
     this.TimeEnrtyForm = this.fb.group({ date: [''], uninvoicedWork: [''], dlpdrop: [''], });
     if (this.lastFilter) {
-      if (this.lastFilter.ItemDateStart && this.lastFilter.ItemDateEnd) {
-        let tempDate = this.lastFilter.ItemDateStart.split("/");
-        let tempDate2 = this.lastFilter.ItemDateEnd.split("/");
-        let Sd = new Date(tempDate[1] + '/' + tempDate[0] + '/' + tempDate[2]);
-        let ed = new Date(tempDate2[1] + '/' + tempDate2[0] + '/' + tempDate2[2]);
-        this.TimeEnrtyForm.controls['date'].setValue({ begin: Sd, end: ed });
-      } else {
-        var dt = new Date();
-        dt.setMonth(dt.getMonth() + 1);
-        this.TimeEnrtyForm.controls['date'].setValue({ begin: new Date(), end: dt });
-        this.lastFilter.ItemDateStart = this.datepipe.transform(new Date(), 'dd/MM/yyyy');
-        this.lastFilter.ItemDateEnd = this.datepipe.transform(dt, 'dd/MM/yyyy');
-      }
       this.TimeEnrtyForm.controls['uninvoicedWork'].setValue(this.lastFilter.Invoiced);
       this.TimeEnrtyForm.controls['dlpdrop'].setValue(this.lastFilter.FeeEarner);
     } else {
-      var dt = new Date();
-      dt.setMonth(dt.getMonth() + 1);
-      this.TimeEnrtyForm.controls['date'].setValue({ begin: new Date(), end: dt });
-      this.lastFilter = {'MatterGuid':this.currentMatter.MATTERGUID ,'FeeEarner': '', 'Invoiced': "", 'ItemDateStart': this.datepipe.transform(new Date(), 'dd/MM/yyyy'), 'ItemDateEnd': this.datepipe.transform(dt, 'dd/MM/yyyy') };
+      this.lastFilter = { 'MatterGuid': this.currentMatter.MATTERGUID, 'FeeEarner': '', 'Invoiced': "", 'ItemDateStart': '', 'ItemDateEnd': '' };
       localStorage.setItem('Work_in_progress_filter', JSON.stringify(this.lastFilter));
     }
 
   }
 
-  WorkInProgressdata:any
+  WorkInProgressdata: any
   ngOnInit() {
     $('content').addClass('inner-scroll');
     $('.example-containerdata').css('height', ($(window).height() - ($('#tool_baar_main').height() + 140)) + 'px');
     this.getTableFilter();
-    // console.log(this.currentMatter.MATTERGUID );
-    // console.log(this.lastFilter)
     this.loadData(this.lastFilter);
   }
   getTableFilter() {
@@ -78,12 +60,10 @@ export class WorkInProgressComponent implements OnInit {
     });
   }
   loadData(potData) {
-    console.log(potData);
-    potData.MatterGuid=this.currentMatter.MATTERGUID
+    potData.MatterGuid = this.currentMatter.MATTERGUID
     this.isLoadingResults = true;
     // let potData = { 'MatterGuid': this.currentMatter.MATTERGUID };
     this.WorkInProgress.WorkInProgressData(potData).subscribe(res => {
-      console.log(res);
       if (res.CODE == 200 && res.STATUS == "success") {
         this.WorkInProgressdata = new MatTableDataSource(res.DATA.WORKITEMS);
         this.WorkInProgressdata.paginator = this.paginator;
@@ -100,7 +80,7 @@ export class WorkInProgressComponent implements OnInit {
     localStorage.setItem('lastPageSize', event.pageSize);
   }
   uninvoicedWorkChange(value) {
-    let filterVal = {'MatterGuid':this.currentMatter.MATTERGUID, 'FeeEarner': '', 'Invoiced': value, 'ItemDateStart': '', 'ItemDateEnd': '' };
+    let filterVal = { 'MatterGuid': this.currentMatter.MATTERGUID, 'FeeEarner': '', 'Invoiced': value, 'ItemDateStart': '', 'ItemDateEnd': '' };
     if (!localStorage.getItem('Work_in_progress_filter')) {
       localStorage.setItem('Work_in_progress_filter', JSON.stringify(filterVal));
     } else {
@@ -111,7 +91,7 @@ export class WorkInProgressComponent implements OnInit {
     this.loadData(filterVal);
   }
   dlpChange(value) {
-    let filterVal = { 'MatterGuid':this.currentMatter.MATTERGUID,'FeeEarner': value, 'Invoiced': '', 'ItemDateStart': '', 'ItemDateEnd': '' };
+    let filterVal = { 'MatterGuid': this.currentMatter.MATTERGUID, 'FeeEarner': value, 'Invoiced': '', 'ItemDateStart': '', 'ItemDateEnd': '' };
     if (!localStorage.getItem('Work_in_progress_filter')) {
       localStorage.setItem('Work_in_progress_filter', JSON.stringify(filterVal));
     } else {
@@ -126,15 +106,10 @@ export class WorkInProgressComponent implements OnInit {
   choosedDate(type: string, event: MatDatepickerInputEvent<Date>) {
     let begin = this.datepipe.transform(event.value['begin'], 'dd/MM/yyyy');
     let end = this.datepipe.transform(event.value['end'], 'dd/MM/yyyy');
-    let filterVal = { 'FeeEarner': '', 'Invoiced': '', 'ItemDateStart': begin, 'ItemDateEnd': end };
-    if (!localStorage.getItem('Work_in_progress_filter')) {
-      localStorage.setItem('Work_in_progress_filter', JSON.stringify(filterVal));
-    } else {
-      filterVal = JSON.parse(localStorage.getItem('Work_in_progress_filter'));
-      filterVal.ItemDateStart = begin;
-      filterVal.ItemDateEnd = end;
-      localStorage.setItem('Work_in_progress_filter', JSON.stringify(filterVal));
-    }
+    let filterVal = { 'FeeEarner': '', 'Invoiced': '', 'ItemDateStart': '', 'ItemDateEnd': '' };
+    filterVal = JSON.parse(localStorage.getItem('Work_in_progress_filter'));
+    filterVal.ItemDateStart = begin;
+    filterVal.ItemDateEnd = end;
     this.loadData(filterVal);
   }
 

c297facfb2438434262b67cef888b181f354e8f8
MOONTECH\kalpesh <kalpesh>
2019-06-22 15:25:19 +0530

    matter detail issue fix

--- src/app/main/pages/invoice/invoice-add-dailog/invoice-add-dailog.component.ts
+++ src/app/main/pages/invoice/invoice-add-dailog/invoice-add-dailog.component.ts
@@ -116,14 +116,12 @@ export class InvoiceAddDailogComponent implements OnInit {
     });
     this.isLoadingResults = false;
   }
-  firstDate(val) {
-    var dt = val._d;
-    dt.setMonth(dt.getMonth() + 1);
-    this.addInvoiceForm.controls['DUEDATETEXT'].setValue(dt);
-    this.addInvoiceForm.controls['DUEDATE'].setValue(dt);
-  }
   invoiceDateChange(type: string, event: MatDatepickerInputEvent<Date>) {
     this.addInvoiceForm.controls['INVOICEDATE'].setValue(this.datepipe.transform(event.value, 'dd/MM/yyyy'));
+    var dt = event.value;
+    dt.setMonth(dt.getMonth() + 1);
+    this.addInvoiceForm.controls['DUEDATETEXT'].setValue(new Date(dt));
+    this.addInvoiceForm.controls['DUEDATE'].setValue(this.datepipe.transform(dt, 'dd/MM/yyyy'));
   }
   dueDateChange(type: string, event: MatDatepickerInputEvent<Date>) {
     this.addInvoiceForm.controls['DUEDATE'].setValue(this.datepipe.transform(event.value, 'dd/MM/yyyy'));
--- src/app/main/pages/matters/matters-sort-detail/matters-sort-detail.component.ts
+++ src/app/main/pages/matters/matters-sort-detail/matters-sort-detail.component.ts
@@ -4,7 +4,7 @@ import { FuseSidebarService } from '@fuse/components/sidebar/sidebar.service';
 import * as $ from 'jquery';
 import { ToastrService } from 'ngx-toastr';
 import { isValid } from 'date-fns';
-import { TimersService } from '../../../../_services'
+import { TimersService, MattersService } from '../../../../_services'
 import { MatterPopupComponent } from '../matter-popup/matter-popup.component';
 import { MatDialogConfig, MatDialog } from '@angular/material';
 @Component({
@@ -22,11 +22,14 @@ export class MattersSortDetailComponent implements OnInit {
   contectTitle = this.theme_type == "theme-default" ? 'Solicitor' : 'Client';
   prevMatterArray: any[] = [];
   constructor(
-    private _fuseSidebarService: FuseSidebarService, private toastr: ToastrService, private TimersServiceI: TimersService, public dialog: MatDialog
+    private _fuseSidebarService: FuseSidebarService,
+    private toastr: ToastrService,
+    private TimersServiceI: TimersService,
+    public dialog: MatDialog,
+    private _mattersService: MattersService,
   ) { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
   closeDetail(event) {
     this.matterCloseD.emit(event);

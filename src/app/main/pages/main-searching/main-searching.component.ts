38d8e2d8d6ef02aa69170c15eb6a6f6653c777fd
Gunjan D <gunjan@moontechnolabs.com>
2019-09-11 17:06:27 +0530

    search and conflict check

--- src/app/main/pages/main-searching/main-searching.component.ts
+++ src/app/main/pages/main-searching/main-searching.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { fuseAnimations } from '@fuse/animations';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { MatterPopupComponent } from '../matters/matter-popup/matter-popup.component';
-import { MatDialog } from '@angular/material';
+import { MatDialog, MatPaginator, MatSort, MatTableDataSource, MatDatepickerInputEvent } from '@angular/material';
 import { MatterDialogComponent } from '../time-entries/matter-dialog/matter-dialog.component';
 import * as $ from 'jquery';
-import { MainAPiServiceService } from 'app/_services';
+import { MainAPiServiceService ,TableColumnsService} from 'app/_services';
 import { ToastrService } from 'ngx-toastr';
+import { DatePipe } from '@angular/common';
 
 
 @Component({
@@ -17,12 +18,23 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class MainSearchingComponent implements OnInit {
   MainSearching:FormGroup;
-  @Input() errorWarningData: any;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
   isLoadingResults: boolean = false;
   addData:any=[];
+  highlightedRows: any;
+  MainSearchingData:any=[];
+  ColumnsObj = [];
+  pageSize: any;
+  displayedColumns: string[];
+  tempColobj: any;
+  filterData: { 'MATTERGUID': any, 'STATUS': any; 'Search': string;  'DUEDATEFROM': any; 'DUEDATETO': any, "Matter": any };
+  ImgDisAb: string;
   constructor(private dialog: MatDialog,private _formBuilder: FormBuilder,
-  private _mainAPiServiceService: MainAPiServiceService,
-  private toastr: ToastrService,) { }
+  private _mainAPiServiceService: MainAPiServiceService,private TableColumnsService: TableColumnsService,
+  private toastr: ToastrService,public datepipe: DatePipe,) {
+    this.getTableFilter();
+   }
 
   ngOnInit() {
     $('.example-containerdata').css('height', ($(window).height() - ($('#tool_baar_main').height() + $('.sticky_search_div').height() + 80)) + 'px');
@@ -30,29 +42,53 @@ export class MainSearchingComponent implements OnInit {
       matterCheck: [''],
       active: [''],
       status:[''],
-      matter:['']
-    
+      matter:[''],
+      DateRange:['']
     });
-    this.LoadData();
-    this.MainSearching.controls['matterCheck'].setValue(true);
-    this.MainSearching.controls['status'].setValue('all');
-    this.MainSearching.controls['matter'].disable();
+   
+    this.filterData = {
+      'MATTERGUID': '', 'Search': '', 'DUEDATEFROM': '', 'DUEDATETO': '', 'STATUS': ' ', "Matter": ''
+    }
+    //  this.filterData.DUEDATEFROM=new Date();
+    //  this.filterData.DUEDATETO=new Date();
+    if (!localStorage.getItem("search_filter")) {
+      localStorage.setItem('search_filter', JSON.stringify(this.filterData));
+    } else {
+      this.filterData = JSON.parse(localStorage.getItem("search_filter"));
+    }
+    this.MainSearching.controls['status'].setValue(this.filterData.STATUS);
+    this.MainSearching.controls['matter'].setValue(this.filterData.Matter);
+    let date = this.filterData.DUEDATEFROM.split("/");
+    let putDate1 = new Date(date[1] + '/' + date[0] + '/' + date[2]);
+    let date2 = this.filterData.DUEDATETO.split("/");
+    let putDate2 = new Date(date2[1] + '/' + date2[0] + '/' + date2[2]);
+    this.MainSearching.controls['DateRange'].setValue({ begin: putDate1, end: putDate2 });
+    if (this.filterData.MATTERGUID == '') {
+      this.MainSearching.controls['matterCheck'].setValue(true);
+      this.MainSearching.controls['matter'].disable();
+      this.CheckboxChecxed();
+    }else{
+      this.LoadData(this.filterData);
+    }
+    
+    // this.MainSearching.controls['status'].setValue('all');
+   
   }
-  LoadData() {
-  
+  LoadData(data) {
+  this.MainSearchingData=[];
       this.isLoadingResults = true;
-      this._mainAPiServiceService.getSetData({}, 'GetCostRecovery').subscribe(res => {
+      this._mainAPiServiceService.getSetData(data, 'GetCostRecovery').subscribe(res => {
         console.log(res);
-        // this.TaskAllData = new MatTableDataSource(res.DATA.TASKS);
-        // this.TaskAllData.sort = this.sort;
-        // this.TaskAllData.paginator = this.paginator;
+        this.MainSearchingData = new MatTableDataSource(res.DATA.TASKS);
+        this.MainSearchingData.sort = this.sort;
+        this.MainSearchingData.paginator = this.paginator;
         if (res.CODE == 200 && res.STATUS == "success") {
-          if (res.DATA.TASKS[0]) {
-            // this.behaviorService.TaskData(res.DATA.TASKS[0]);
-            // this.highlightedRows = res.DATA.TASKS[0].TASKGUID;
-          } else {
-            // this.toastr.error("No Data Selected");
-          }
+          // if (res.DATA.TASKS[0]) {
+          //   // this.behaviorService.TaskData(res.DATA.TASKS[0]);
+          // this.highlightedRows = res.DATA.TASKS[0].TASKGUID;
+          // } else {
+          //   // this.toastr.error("No Data Selected");
+          // }
           this.isLoadingResults = false;
         }
       }, err => {
@@ -60,54 +96,108 @@ export class MainSearchingComponent implements OnInit {
         this.toastr.error(err);
   
       });
-      // this.pageSize = localStorage.getItem('lastPageSize');
+       this.pageSize = localStorage.getItem('lastPageSize');
+    }
+    getTableFilter() {
+      this.TableColumnsService.getTableFilter('searching', '').subscribe(response => {
+        if (response.CODE == 200 && response.STATUS == "success") {
+          let data = this.TableColumnsService.filtertableColum(response.DATA.COLUMNS);
+          this.tempColobj = data.tempColobj;
+          this.displayedColumns = data.showcol;
+          this.ColumnsObj = data.colobj;
+        }
+      }, error => {
+        this.toastr.error(error);
+      });
     }
   get f() {
     //console.log(this.contactForm);
     return this.MainSearching.controls;
   }
-  CheckboxChecxed(){
-    if(this.f.matterCheck.value == true){
-      this.MainSearching.controls['matter'].disable();
-    }else{
-      this.MainSearching.controls['matter'].enable();
-      const dialogRef = this.dialog.open(MatterDialogComponent, { width: '100%', disableClose: true, data: null });
-      dialogRef.afterClosed().subscribe(result => {
+//   CheckboxChecxed(){
+//     if(this.f.matterCheck.value == true){
+//       this.MainSearching.controls['matter'].disable();
+//     }else{
+//       this.MainSearching.controls['matter'].enable();
+//       const dialogRef = this.dialog.open(MatterDialogComponent, { width: '100%', disableClose: true, data: null });
+//       dialogRef.afterClosed().subscribe(result => {
        
-          if (result) {
-              localStorage.setItem('set_active_matters', JSON.stringify(result));
-              this.MainSearching.controls['matter'].setValue(result.MATTER);      
-          }
-          else if (this.f.matter.value==''){
-            this.MainSearching.controls['matterCheck'].setValue(true);
-          }
-      }); 
-  }
-}
-  SelectMatter(){
+//           if (result) {
+//               localStorage.setItem('set_active_matters', JSON.stringify(result));
+//               this.MainSearching.controls['matter'].setValue(result.MATTER);      
+//           }
+//           else if (this.f.matter.value==''){
+//             this.MainSearching.controls['matterCheck'].setValue(true);
+//           }
+//       }); 
+//   }
+// }
+CheckboxChecxed(){
+  if(this.f.matterCheck.value == true) {
+    this.ImgDisAb = "menu-disabled";
+    this.MainSearching.controls['matter'].disable();
+    this.filterData = JSON.parse(localStorage.getItem("search_filter"));
+    this.filterData.MATTERGUID = "";
+    localStorage.setItem('search_filter',JSON.stringify(this.filterData));
+    this.LoadData(this.filterData);
+  } else {
+    this.ImgDisAb = "";
+    this.MainSearching.controls['matter'].enable();
     const dialogRef = this.dialog.open(MatterDialogComponent, { width: '100%', disableClose: true, data: null });
-      dialogRef.afterClosed().subscribe(result => {
-          if (result) {
-            
-              localStorage.setItem('set_active_matters', JSON.stringify(result));
-              this.MainSearching.controls['matter'].setValue(result.MATTER);  
-          }
-      }); 
-//     let mattersData = JSON.parse(localStorage.getItem('set_active_matters'));
-//     let MaterPopupData = { action: 'edit', 'matterGuid': mattersData.MATTERGUID }
-// //    let contactPopupData = { action:'edit' };
-//     const dialogRef = this.dialog.open(MatterPopupComponent, {
-//         disableClose: true, panelClass: 'contact-dialog', data: MaterPopupData
-//     });
-//     dialogRef.afterClosed().subscribe(result => {
-//       console.log(result);
-//     // this.SearchForm.controls['Matter'].setValue(result);  
-//     });
-  }
-  DateRange(){
+    dialogRef.afterClosed().subscribe(result => {
+      if(result != false){
+        if (result) {
+          localStorage.setItem('set_active_matters', JSON.stringify(result));
+          this.MainSearching.controls['matter'].setValue(result.MATTER);
+          this.filterData = JSON.parse(localStorage.getItem("search_filter"));
+          this.filterData.MATTERGUID = result.MATTERGUID;
+          this.filterData.Matter = result.MATTER;
+          localStorage.setItem('search_filter', JSON.stringify(this.filterData));
+          this.LoadData(this.filterData);
+        }
+        else if (this.f.matter.value == '') {
+          this.MainSearching.controls['matterCheck'].setValue(true);
+        }
+      }else{
+        this.ImgDisAb = "menu-disabled";
+        this.MainSearching.controls['matterCheck'].setValue(true);
+        this.MainSearching.controls['matter'].disable();
+      }
 
+    });
   }
-DateRange1(){
-  
 }
+
+  SelectMatter() {
+    const dialogRef = this.dialog.open(MatterDialogComponent, { width: '100%', disableClose: true, data: null });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log(result);
+        this.MainSearching.controls['matter'].setValue(result.MATTER);
+        this.filterData = JSON.parse(localStorage.getItem("search_filter"));
+        this.filterData.MATTERGUID = result.MATTERGUID;
+        this.filterData.Matter = result.MATTER;
+        localStorage.setItem('search_filter', JSON.stringify(this.filterData));
+        this.LoadData(this.filterData);
+      }
+    });
+  }
+  DateRange1(type: string, event: MatDatepickerInputEvent<Date>) {
+    let begin = this.datepipe.transform(event.value['begin'], 'dd/MM/yyyy');
+    let end = this.datepipe.transform(event.value['end'], 'dd/MM/yyyy');
+    this.filterData = JSON.parse(localStorage.getItem("search_filter"));
+    this.filterData.DUEDATEFROM = begin;
+    this.filterData.DUEDATETO = end;
+    localStorage.setItem('search_filter', JSON.stringify(this.filterData));
+    this.LoadData(this.filterData);
+  }
+  DateRange(a, b) {
+  }
+  selectStatus(val) {
+    this.filterData = JSON.parse(localStorage.getItem("search_filter"));
+    this.filterData.STATUS = val;
+    localStorage.setItem('search_filter',JSON.stringify(this.filterData));
+    this.LoadData(this.filterData);
+  }
+
 }

5d863adef049fe3224af3fff7538d8196717c881
kalpeshmoon <kalpesh@moontechnolabs.com>
2019-10-02 12:28:32 +0530

    Timer add on left side

--- src/app/layout/components/quick-panel/quick-panel.component.ts
+++ src/app/layout/components/quick-panel/quick-panel.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnDestroy, OnInit, ViewEncapsulation, Input, Output, EventEmitter } from '@angular/core';
 import { Subject } from 'rxjs';
 import * as $ from 'jquery';
+import { MatDialogRef, MatDialog } from '@angular/material';
+import { TimersService } from 'app/_services';
+import { MatterDialogComponent } from 'app/main/pages/time-entries/matter-dialog/matter-dialog.component';
 
 @Component({
     selector: 'quick-panel',
@@ -15,8 +18,9 @@ export class QuickPanelComponent implements OnInit, OnDestroy {
     @Output() StopMatterId: EventEmitter<any> = new EventEmitter<any>();
     @Output() EndMatterId: EventEmitter<any> = new EventEmitter<any>();
     @Output() StartMatterId: EventEmitter<any> = new EventEmitter<any>();
-    constructor() {
+    constructor(private TimersServiceI: TimersService, public MatDialog: MatDialog) {
         this._unsubscribeAll = new Subject();
+
     }
 
 
@@ -35,5 +39,15 @@ export class QuickPanelComponent implements OnInit, OnDestroy {
     }
     endTimer(MatterId: any) {
         this.EndMatterId.emit(MatterId);
+    }
+    AddTimer() {
+        const dialogRef = this.MatDialog.open(MatterDialogComponent, { width: '100%', disableClose: true, data: {} });
+        dialogRef.afterClosed().subscribe(result => {
+            if (result) {
+                localStorage.setItem('set_active_matters', JSON.stringify(result));
+                this.TimersServiceI.addTimeEnrtS('');
+            }
+        });
+
     }
 }

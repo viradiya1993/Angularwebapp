325241ba761fc896e63e4bbc17cb244be3789aaa
kalpeshmoon <kalpesh@moontechnolabs.com>
2019-10-09 10:59:51 +0530

    enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,11 @@ export const environment = {
     ReportUrl: 'https://apitest.silq.com.au',
     APIKEY: 'SNGMTUEEB2AJBFC9',
     VERSION: '1.5.0'
+    // production: false,
+    // hmr: false,
+    // APIEndpoint: 'https://api2.silq.com.au/',
+    // ReportUrl: 'https://api2.silq.com.au/',
+    // APIKEY: 'SNGMTUEEB2AJBFC9',
+    // VERSION: '1.5.0'
     // APIEndpoint: 'https://api.silq.com.au/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
     APIEndpoint: 'https://apitest.silq.com.au/',
     ReportUrl: 'https://apitest.silq.com.au',
     APIKEY: 'SNGMTUEEB2AJBFC9',
-    VERSION: '1.5.0'
+    VERSION: '1.5.0',
 };
 

9ab309233f311bdf54205378fa65e2bddd758ccc
kalpeshmoon <kalpesh@moontechnolabs.com>
2019-10-18 10:20:35 +0530

    set new version in env

--- src/environments/environment.sandbox.ts
+++ src/environments/environment.sandbox.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     hmr: false,
     APIEndpoint: 'https://apitest.silq.com.au/',
-    ReportUrl: 'https://apitest.silq.com.au',
+    ReportUrl: 'https://apitest.silq.com.au/',
     APIKEY: 'SNGMTUEEB2AJBFC9',
-    VERSION: '1.5.2'
+    VERSION: '1.5.3'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: false,
     hmr: false,
     APIEndpoint: 'https://apitest.silq.com.au/',
-    ReportUrl: 'https://apitest.silq.com.au',
+    ReportUrl: 'https://apitest.silq.com.au/',
     APIKEY: 'SNGMTUEEB2AJBFC9',
-    VERSION: '1.5.2',
+    VERSION: '1.5.3',
 };
 

24664b3878b9349ba21d9eea1b6839f4177a5b32
kalpeshmoon <kalpesh@moontechnolabs.com>
2019-10-22 16:35:00 +0530

    change version of app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
     APIEndpoint: 'https://apitest.silq.com.au/',
     ReportUrl: 'https://apitest.silq.com.au/',
     APIKEY: 'SNGMTUEEB2AJBFC9',
-    VERSION: '1.5.4',
+    VERSION: '1.5.5',
 };
 

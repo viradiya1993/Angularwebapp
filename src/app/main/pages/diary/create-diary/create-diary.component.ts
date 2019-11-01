e4cfba0090a09c32721dec5b3a654c62069705a7
Gunjan D <gunjan@moontechnolabs.com>
2019-11-01 14:13:22 +0530

    dairy

--- src/app/main/pages/diary/create-diary/create-diary.component.ts
+++ src/app/main/pages/diary/create-diary/create-diary.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, Input, ViewChild, ViewEncapsulation } from '@angular/core';
 import { fuseAnimations } from '@fuse/animations';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { MainAPiServiceService, TimersService, TableColumnsService, BehaviorService } from './../../../../_services';
 import { ToastrService } from 'ngx-toastr';
 import { SelectionModel } from '@angular/cdk/collections';
-import { MatTableDataSource, MatPaginator, MatSort, MatDatepickerInputEvent } from '@angular/material';
+import { MatTableDataSource, MatPaginator, MatSort, MatDatepickerInputEvent, MatDialog } from '@angular/material';
 import { DatePipe } from '@angular/common';
+import { MatterDialogComponent } from '../../time-entries/matter-dialog/matter-dialog.component';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-create-diary',
@@ -15,12 +17,20 @@ import { DatePipe } from '@angular/common';
   animations: fuseAnimations
 })
 export class CreateDiaryComponent implements OnInit {
+  CreateTimeEnrtyForm: FormGroup;
   isLoadingResults: boolean = false;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   tempColobj: any;
   ColumnsObj = [];
+  timeStops: any = [];
+  userList: any;
+  SendCreateTimeEntries:any=[];
+  ActivityList: any;
+  LookupsList: any;
   createDiaryForm: any = {}
+  currenbtMatter=JSON.parse(localStorage.getItem('set_active_matters'));
+  calculateData: any = { MatterGuid: '', QuantityType: '', Quantity: '', FeeEarner: '', FeeType: '' };
   // createDiaryForm={
   //   'Date Range':'','Date':'','Item':''
   // }
@@ -35,9 +45,12 @@ export class CreateDiaryComponent implements OnInit {
   pageSize: string;
   isDisplay: boolean = false;
   CreateDiaryArray: any=[];
+  INDEX: any;
   constructor(private _mainAPiServiceService: MainAPiServiceService,
     private Timersservice: TimersService,
     public datepipe: DatePipe,
+    private fb: FormBuilder,
+    private dialog: MatDialog,
     private behaviorService: BehaviorService,
     private TableColumnsService: TableColumnsService,
     private toastr: ToastrService, ) {
@@ -46,6 +59,55 @@ export class CreateDiaryComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.CreateTimeEnrtyForm = this.fb.group({ 
+      matterautoVal: [''], 
+      MATTERGUID:[''],
+      ADDITIONALTEXT:[''],
+
+
+      PRICEINCGST:[''],
+      PRICE:[''],
+      QUANTITYTYPE:[''],
+      QUANTITY:[''],
+      FEEEARNER:[''],
+      ITEMTIME:[''],
+      ITEMDATETEXT:[''],
+      ITEMDATE:[''],
+     
+      uninvoicedWork: [''],
+       dlpdrop: [''], 
+    });
+    this.Timersservice.GetUsers({}).subscribe(res => {
+      if (res.CODE == 200 && res.STATUS == "success") {
+        this.userList = res.DATA.USERS;
+      } else {
+        this.userList = [];
+      }
+    }, err => {
+      this.toastr.error(err);
+    });
+    this.ActivityList = [
+      { 'ACTIVITYID': 'X', 'DESCRIPTION': 'hh:mm' }, { 'ACTIVITYID': 'H', 'DESCRIPTION': 'Hours' },
+      { 'ACTIVITYID': 'M', 'DESCRIPTION': 'Minutes' }, { 'ACTIVITYID': 'D', 'DESCRIPTION': 'Days' },
+      { 'ACTIVITYID': 'U', 'DESCRIPTION': 'Units' }, { 'ACTIVITYID': 'F', 'DESCRIPTION': 'Fixed' }
+      // { 'ACTIVITYID': 'hh:mm', 'DESCRIPTION': 'hh:mm' }, { 'ACTIVITYID': 'Hours', 'DESCRIPTION': 'Hours' },
+      // { 'ACTIVITYID': 'Minutes', 'DESCRIPTION': 'Minutes' }, { 'ACTIVITYID': 'Days', 'DESCRIPTION': 'Days' },
+      // { 'ACTIVITYID': 'Units', 'DESCRIPTION': 'Units' }, { 'ACTIVITYID': 'Fixed', 'DESCRIPTION': 'Fixed' }
+    ];
+    this.Timersservice.GetLookupsData({}).subscribe(res => {
+      if (res.CODE == 200 && res.STATUS == "success") {
+        this.LookupsList = res.DATA.LOOKUPS;
+      } else {
+        this.LookupsList = [];
+      }
+      this.isLoadingResults = false;
+    }, err => {
+      this.toastr.error(err);
+    });
+    this.calculateData.MatterGuid=this.currenbtMatter.MATTERGUID;
+    this.calculateData.QuantityType = 'H';
+    this.timeStops = this.getTimeStops('00:00', '23:30');
+
     this.createDiaryForm.Date = ({ begin: new Date(), end: new Date() });
     this.createDiaryForm.Search = "";
     this.createDiaryForm.Item = 'All Items';
@@ -53,6 +115,18 @@ export class CreateDiaryComponent implements OnInit {
   helloFunction() {
     console.log(this.selection.selected);
 
+    this.selection.selected.forEach(element => {
+      this.SendCreateTimeEntries.push({
+        PRICE:element.PRICE,
+        PRICEINCGST:element.PRICEINCGST,
+        ITEMDATE:element.APPOINTMENTDATE,
+        ITEMTIME:element.APPOINTMENTTIME,
+        MATTERGUID:element.MATTERGUID,
+        FEEEARNER:element.FEEEARNERID,
+        APPOINTMENTGUID:element.APPOINTMENTGUID,
+        QUANTITY:element.QUANTITY,
+      });
+    });
 
   }
   isAllSelected() {
@@ -112,6 +186,19 @@ export class CreateDiaryComponent implements OnInit {
         this.TimerDataFordiary = new MatTableDataSource(response.DATA.APPOINTMENTS);
           this.TimerDataFordiary.paginator = this.paginator;
           this.TimerDataFordiary.sort = this.sort;
+          // response.DATA.APPOINTMENTS.forEach(element => {
+          //   this.SendCreateTimeEntries.push({
+          //     PRICE:element.PRICE,
+          //     PRICEINCGST:element.PRICEINCGST,
+          //     ITEMDATE:element.APPOINTMENTDATE,
+          //     ITEMTIME:element.APPOINTMENTTIME,
+          //     MATTERGUID:element.MATTERGUID,
+          //     FEEEARNER:element.FEEEARNERID,
+          //     APPOINTMENTGUID:element.APPOINTMENTGUID,
+          //     QUANTITY:element.QUANTITY,
+          //   });
+          // });
+        
         if (response.DATA.APPOINTMENTS[0]) {
           // this.behaviorService.MainTimeEntryData(response.DATA.WORKITEMS[0]);
            this.isDisplay = false;
@@ -134,6 +221,100 @@ export class CreateDiaryComponent implements OnInit {
     });
     this.pageSize = localStorage.getItem('lastPageSize');
   }
+  public selectMatter() {
+    const dialogRef = this.dialog.open(MatterDialogComponent, { width: '100%', disableClose: true, data: null });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.CreateTimeEnrtyForm.controls['MATTERGUID'].setValue(result.MATTERGUID);
+        this.CreateTimeEnrtyForm.controls['matterautoVal'].setValue(result.SHORTNAME + ' : ' + result.MATTER);
+        this.matterChange('MatterGuid', result.MATTERGUID);
+      }
+    });
+  }
+  QuickDate(type: string, event: MatDatepickerInputEvent<Date>) {
+  this.CreateTimeEnrtyForm.controls['ITEMDATE'].setValue(this.datepipe.transform(event.value, 'dd/MM/yyyy'));
+  this.CommonChagesArray();
+
+  }
+  getTimeStops(start, end) {
+    var startTime = moment(start, 'hh:mm');
+    var endTime = moment(end, 'hh:mm');
+    if (endTime.isBefore(startTime)) {
+      endTime.add(1, 'day');
+    }
+    var timeStops = [];
+    while (startTime <= endTime) {
+      timeStops.push(moment(startTime).format('hh:mm A'));
+      startTime.add(30, 'minutes');
+    }
+    return timeStops;
+  }
+  calcPE() {
+    this.CreateTimeEnrtyForm.controls['PRICEINCGST'].setValue((this.qf.PRICE.value * 1.1).toFixed(2));
+  }
+  calcPI() {
+    this.CreateTimeEnrtyForm.controls['PRICE'].setValue((this.qf.PRICEINCGST.value / 1.1).toFixed(2));
+  }  
+  matterChange(key: any, event: any) {
+    console.log(key);
+    console.log(event);
+    if (key == "MatterGuid") {
+      this.CreateTimeEnrtyForm.controls['MATTERGUID'].setValue(event);
+      this.calculateData.MatterGuid = event;
+    } else if (key == "FeeEarner") {
+      this.calculateData.FeeEarner = event;
+    } else if (key == "QuantityType") {
+      switch (event) {
+        case 'X': {
+          this.calculateData.QuantityType = 'X';
+          break;
+        }
+        case 'H': {
+          this.calculateData.QuantityType = 'H';
+          break;
+        }
+        case 'M': {
+          this.calculateData.QuantityType = 'M';
+          break;
+        }
+        case 'D': {
+          this.calculateData.QuantityType = 'D';
+          break;
+        }
+        case 'U': {
+          this.calculateData.QuantityType = 'U';
+          break;
+        }
+        case 'F': {
+          this.calculateData.QuantityType = 'F';
+          break;
+        }
+        default: {
+          this.calculateData.FeeType = event;
+          this.calculateData.QuantityType = 'F';
+          break;
+        }
+       
+      }
+    }
+    this.calculateData.Quantity = this.qf.QUANTITY.value;
+    if (this.calculateData.MatterGuid != '' && this.calculateData.Quantity != '' && (this.calculateData.QuantityType != '' || this.calculateData.FeeType != '')) {
+      this.isLoadingResults = true;
+      this.Timersservice.calculateWorkItems(this.calculateData).subscribe(response => {
+        if (response.CODE == 200 && response.STATUS == "success") {
+          let CalcWorkItemCharge = response.DATA;
+          this.CreateTimeEnrtyForm.controls['PRICE'].setValue(CalcWorkItemCharge.PRICE);
+          this.CreateTimeEnrtyForm.controls['PRICEINCGST'].setValue(CalcWorkItemCharge.PRICEINCGST);
+          this.isLoadingResults = false;
+        }
+      }, err => {
+        this.isLoadingResults = false;
+        this.toastr.error(err);
+      });
+    }
+  }  get qf() {
+    return this.CreateTimeEnrtyForm.controls;
+  }
   DateRange1(type: string, event: MatDatepickerInputEvent<Date>) {
 
     let begin = this.datepipe.transform(event.value['begin'], 'dd/MM/yyyy');
@@ -144,9 +325,41 @@ export class CreateDiaryComponent implements OnInit {
   }
   DateRange(a, b) {
   }
-  Rowclick(val) {
-    this.behaviorService.MainTimeEntryData(val);
-    localStorage.setItem('edit_WORKITEMGUID', val.WORKITEMGUID);
+  Rowclick(val,index) {
+    this.INDEX=index;
+    // this.behaviorService.MainTimeEntryData(val);
+    // localStorage.setItem('edit_WORKITEMGUID', val.WORKITEMGUID);
+    this.CreateTimeEnrtyForm.controls['PRICE'].setValue(val.PRICE);
+    this.CreateTimeEnrtyForm.controls['PRICEINCGST'].setValue(val.PRICEINCGST);
+    this.CreateTimeEnrtyForm.controls['matterautoVal'].setValue(this.currenbtMatter.SHORTNAME);
+    this.CreateTimeEnrtyForm.controls['MATTERGUID'].setValue(this.currenbtMatter.MATTERGUID);
+    let Date1 = val.APPOINTMENTDATE.split("/");
+    let ShowDate = new Date(Date1[1] + '/' + Date1[0] + '/' + Date1[2]);
+    this.CreateTimeEnrtyForm.controls['ITEMDATETEXT'].setValue(ShowDate);
+    this.CreateTimeEnrtyForm.controls['ITEMDATE'].setValue(val.APPOINTMENTDATE);
+    let ttyData = moment(val.APPOINTMENTTIME, 'hh:mm');
+    this.CreateTimeEnrtyForm.controls['ITEMTIME'].setValue(moment(ttyData).format('hh:mm A'));
+    // this.CreateTimeEnrtyForm.controls['ITEMTIME'].setValue(val.APPOINTMENTTIME);
+    this.CreateTimeEnrtyForm.controls['FEEEARNER'].setValue(val.FEEEARNERID);
+    this.CreateTimeEnrtyForm.controls['QUANTITY'].setValue(val.QUANTITY);
+    this.CreateTimeEnrtyForm.controls['QUANTITYTYPE'].setValue(val.QUANTITYTYPE);
+    this.CreateTimeEnrtyForm.controls['ADDITIONALTEXT'].setValue('');
+    
+      //chgange in array 
+
+     
+
+
+  }
+
+  CommonChagesArray(){
+    this.SendCreateTimeEntries[this.INDEX].PRICE=this.qf.PRICE.value;
+    this.SendCreateTimeEntries[this.INDEX].PRICEINCGST=this.qf.PRICEINCGST.value;
+    this.SendCreateTimeEntries[this.INDEX].MATTERGUID=this.qf.MATTERGUID.value;
+    this.SendCreateTimeEntries[this.INDEX].ITEMDATE=this.qf.ITEMDATE.value;
+    this.SendCreateTimeEntries[this.INDEX].ITEMTIME=this.qf.ITEMTIME.value;
+    this.SendCreateTimeEntries[this.INDEX].FEEEARNER=this.qf.FEEEARNER.value;
+    this.SendCreateTimeEntries[this.INDEX].QUANTITY=this.qf.QUANTITY.value;
   }
 
   CheckboxClick() {
@@ -156,17 +369,15 @@ export class CreateDiaryComponent implements OnInit {
   saveCreateDiary() {
 
     console.log(this.selection.selected);
-    
-    let finalData = { DATA: this.selection.selected, FormAction: 'insert', VALIDATEONLY: false }
+    console.log(this.SendCreateTimeEntries);
+   
+    let finalData = { DATA: this.SendCreateTimeEntries, FormAction: 'insert', VALIDATEONLY: false }
     this._mainAPiServiceService.getSetData(finalData, 'SetAppointment').subscribe(response => {
       console.log(response);
 
     }, err => {
       this.toastr.error(err);
     });
-
-
-
 
   }
 

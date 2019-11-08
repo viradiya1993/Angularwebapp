c752ab106af70e6631effeded0435fdbc11f6be4
kalpeshmoon <kalpesh@moontechnolabs.com>
2019-11-08 10:52:00 +0530

    version update

--- src/environments/environment.sandbox.ts
+++ src/environments/environment.sandbox.ts
@@ -4,6 +4,6 @@ export const environment = {
     APIEndpoint: 'https://apitest.silq.com.au/',
     ReportUrl: 'https://apitest.silq.com.au/',
     APIKEY: 'SNGMTUEEB2AJBFC9',
-    VERSION: '1.6.1',
+    VERSION: '1.6.2',
     WORDLIMIT: 40,
 };

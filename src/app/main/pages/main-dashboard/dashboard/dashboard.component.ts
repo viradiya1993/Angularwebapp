3d9aea4bcfc0028dc7812bfd6b1a35fd09fde645
Gunjan D <gunjan@moontechnolabs.com>
2019-11-26 14:44:38 +0530

    working on new changes in dashboard

--- src/app/main/pages/main-dashboard/dashboard/dashboard.component.ts
+++ src/app/main/pages/main-dashboard/dashboard/dashboard.component.ts
@@ -8,6 +8,8 @@ import { DashboardService } from './dashboard.service';
 import CanvasJS from 'canvasjs';
 import * as shape from 'd3-shape';
 import { Chart } from 'chart.js';
+import * as moment from 'moment';
+import { DatePipe } from '@angular/common';
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
@@ -16,6 +18,7 @@ import { Chart } from 'chart.js';
 })
 export class DashboardComponent implements OnInit {
     widgets: any;
+    widget11:any;
     chart: any;
     widget2: any = {};
     widget1SelectedYear = '2016';
@@ -23,32 +26,21 @@ export class DashboardComponent implements OnInit {
     isLoadingResults: boolean = false;
     UnbilledTotal: any;
     AgedDebatorTotal: any;
-
-    //doughnut chart
-    //   public chartType: string = 'doughnut';
-    //   public chartLabels: Array<string> = ['January', 'February', 'March'];
-    //   public chartData: Array<number> = [1, 1, 1];
-
-    //   public chartOptions: any = {
-    //     pieceLabel: {
-    //       render: function (args) {
-    //         const label = args.label,
-    //               value = args.value;
-    //         return label + ': ' + value;
-    //       }
-    //     },
-    //     legend: {
-    //         display: true,
-    //         labels: {
-    //             fontColor: 'rgb(255, 99, 132)',
-    //             text:'jkhfgd',
-    //         }~
-    //     }   
-    //   }
-
-
-    ///
-
+    TotalInvoice:any;
+    getMonthNum:any=[];
+    InvoiceInGst:any=[];
+    Childwidget: any;
+    widget12: any;
+    widget13: any;
+    getMonthNumForExpense:any=[];
+    InvoiceInGstForExpense:any=[];
+    getMonthNumForReceipt:any=[];
+    InvoiceInGstForReceipt:any=[]
+    widget5:any
+    deviceValForAged:any=[];
+    widget7:any;
+    deviceValForUnbilled:any=[];
+   
     /**
         * Constructor
         *
@@ -56,7 +48,8 @@ export class DashboardComponent implements OnInit {
         */
     constructor(private _mainAPiServiceService: MainAPiServiceService,
         private toastr: ToastrService, private _analyticsDashboardService: DashboardService,
-        private behaviorService: BehaviorService) {
+        private behaviorService: BehaviorService,
+        public datepipe: DatePipe,) {
 
         //forTotal
         // this.behaviorService.totalDashboard$.subscribe(result => {
@@ -65,19 +58,19 @@ export class DashboardComponent implements OnInit {
         //   });
 
         // Register the custom chart.js plugin
-        this.widget2 = {
-            legend: false,
-            explodeSlices: false,
-            labels: true,
-            doughnut: false,
-            gradient: false,
-            scheme: {
-                domain: ['#f44336', '#9c27b0', '#03a9f4', '#e91e63', '#ffc107']
-            },
-            onSelect: (ev) => {
-                console.log(ev);
-            }
-        };
+        // this.widget2 = {
+        //     legend: false,
+        //     explodeSlices: false,
+        //     labels: true,
+        //     doughnut: false,
+        //     gradient: false,
+        //     scheme: {
+        //         domain: ['#f44336', '#9c27b0', '#03a9f4', '#e91e63', '#ffc107']
+        //     },
+        //     onSelect: (ev) => {
+        //         console.log(ev);
+        //     }
+        // };
         this._registerCustomChartJSPlugin();
         this.chartPl();
 
@@ -87,14 +80,350 @@ export class DashboardComponent implements OnInit {
     selectStatus(val) {
 
     }
+    addMonths(date, months) {
+        date.setMonth(date.getMonth() + months);
+        return date;
+    }
     ngOnInit() {
+        this.widget11="empty";
+        this.widget12="empty";
+        this.widget13="empty";
+        this.widget5="empty";
+        this.widget2="empty";
+        this.widget7="empty";
         // Get the widgets from the service
-        this.widgets = this._analyticsDashboardService.widgets;
-        this.AgedDebatorTotal = this._analyticsDashboardService.AgedDebatorTotal;
-        this.UnbilledTotal = this._analyticsDashboardService.UnbilledTotal;
+        // this.AgedDebatorTotal = this._analyticsDashboardService.AgedDebatorTotal;
+        // this.UnbilledTotal = this._analyticsDashboardService.UnbilledTotal;
+        
+        // this.widgets = this._analyticsDashboardService.widgets;
+        // console.log(this._analyticsDashboardService);
+        this.AgedDebetorData();
+        this.UnbilledWIPData();
+        this.TotalInvoiceData('invoices');
+        this.TotalExpenseData('expenses');
+        this.TotalReceiptData('receipts');
+        // this.ComparisionChart();
+        // this.TotalInvoiceData('expenses');
+        // this.TotalInvoiceData('receipts');
         // this.widgets.widget2 = {
         //     };
     }
+    AgedDebetorData(){
+        this.deviceValForAged = [];
+        this._mainAPiServiceService.getSetData({ Dashboard: 'aged debtors' }, 'GetDashboard').subscribe(res => {
+            if (res.CODE == 200 && res.STATUS == "success") {
+                this.AgedDebatorTotal = (res.DATA.DASHBOARDTOTALS.INCGST).toFixed(2);
+                res.DATA.DASHBOARDDATA.forEach(element => {
+                    this.deviceValForAged.push({ name: element.DATEDESC, value: element.INCGST })
+                });
+                this.widget7= {
+                    scheme : {
+                        domain: ['#4867d2', '#5c84f1', '#89a9f4','#c0caed']
+                    },
+                    devices: this.deviceValForAged
+                }
+            }
+
+        });
+    }
+    UnbilledWIPData(){
+        this.deviceValForUnbilled=[];
+        this._mainAPiServiceService.getSetData({ Dashboard: 'unbilled WIP' }, 'GetDashboard').subscribe(res => {
+            if (res.CODE == 200 && res.STATUS == "success") {
+                this.UnbilledTotal = (res.DATA.DASHBOARDTOTALS.INCGST).toFixed(2);
+                res.DATA.DASHBOARDDATA.forEach(element => {
+                    this.deviceValForUnbilled.push({ name: element.DATEDESC, value: element.INCGST })
+                });
+                this.widget2= {
+                    scheme : {
+                        domain: ['#9205ff', '#b14dff','#d399ff','#6b00bd',]
+                    },
+                    devices:this.deviceValForUnbilled
+                } 
+            }
+        });
+    }
+TotalInvoiceData(type){
+    this.widget11="empty";
+    this.getMonthNum=[];
+    this.InvoiceInGst=[];
+    let MonthStartDate= this.datepipe.transform(this.addMonths(new Date(), -6), 'dd/MM/yyyy');
+    let MonthEndDate= this.datepipe.transform(new Date(), 'dd/MM/yyyy');
+    this._mainAPiServiceService.getSetData({ Dashboard: type,StartDate:MonthStartDate,EndDate:MonthEndDate }, 'GetDashboard').subscribe(res => {
+        console.log(res);
+        if (res.CODE == 200 && res.STATUS == "success") {
+            this.TotalInvoice=res.DATA.DASHBOARDTOTALS.INCGST;
+            res.DATA.DASHBOARDDATA.forEach(element => {
+                element.DATEDESC
+                let MonthWiseDate = element.DATEDESC.split("/");
+                var monthNum = MonthWiseDate[1];   // assuming Jan = 1
+                var shortName = moment.monthsShort(monthNum - 1); 
+                this.getMonthNum.push(shortName);
+                this.InvoiceInGst.push(element.INCGST);
+                
+            });
+            this.widget11= {
+                chartType:"bar",
+                colors:[{backgroundColor: "#42a5f5",
+                borderColor: "#42a5f5"}],
+                conversion:{
+                    value:res.DATA.DASHBOARDTOTALS.INCGST
+                },
+                datasets:[
+                    {data:this.InvoiceInGst,
+                    label:'Total Invoiced'}
+                ],
+                labels:this.getMonthNum,
+                options:{
+                    layout:{
+                        padding:{
+                            bottom: 16,
+                            left: 16,
+                            right: 16,
+                            top: 24
+                        }
+                    },
+                    legend:{display:false},
+                    maintainAspectRatio:false,
+
+                },
+                scales:{
+                    xAxes:[{display:true}],
+                    yAxes:[{display:true ,ticks:{
+                        max:100000000,
+                        min:0,
+                        stepSize:20000000
+                    }},
+                    ]
+                }
+            
+            }
+          
+        }
+    });
+}
+TotalExpenseData(type){
+    this.widget12="empty";
+    this.getMonthNumForExpense=[];
+    this.InvoiceInGstForExpense=[];
+    let MonthStartDate= this.datepipe.transform(this.addMonths(new Date(), -6), 'dd/MM/yyyy');
+    let MonthEndDate= this.datepipe.transform(new Date(), 'dd/MM/yyyy');
+    this._mainAPiServiceService.getSetData({ Dashboard: type,StartDate:MonthStartDate,EndDate:MonthEndDate }, 'GetDashboard').subscribe(res => {
+        console.log(res);
+        if (res.CODE == 200 && res.STATUS == "success") {
+            this.TotalInvoice=res.DATA.DASHBOARDTOTALS.INCGST;
+            res.DATA.DASHBOARDDATA.forEach(element => {
+                element.DATEDESC
+                let MonthWiseDate = element.DATEDESC.split("/");
+                var monthNum = MonthWiseDate[1];   // assuming Jan = 1
+                var shortName = moment.monthsShort(monthNum - 1); 
+                this.getMonthNumForExpense.push(shortName);
+                this.InvoiceInGstForExpense.push(element.INCGST);
+                
+                // invoice vs expense 
+                this.ComparisionChart()
+            });
+            this.widget12= {
+                chartType:"bar",
+                colors:[{backgroundColor: "#ff0000",
+                borderColor: "#42a5f5"}],
+                conversion:{
+                    value:res.DATA.DASHBOARDTOTALS.INCGST
+                },
+                datasets:[
+                    {data:this.InvoiceInGstForExpense,
+                    label:'Total Expense'}
+                ],
+                labels:this.getMonthNumForExpense,
+                options:{
+                    layout:{
+                        padding:{
+                            bottom: 16,
+                            left: 16,
+                            right: 16,
+                            top: 24
+                        }
+                    },
+                    legend:{display:false},
+                    maintainAspectRatio:false,
+
+                },
+                scales:{
+                    xAxes:[{display:true}],
+                    yAxes:[{display:true ,ticks:{
+                        max:100000000,
+                        min:0,
+                        stepSize:20000000
+                    }},
+                    ]
+                }
+            
+            }
+           
+        }
+    });
+}
+TotalReceiptData(type){
+    this.widget13="empty";
+    this.getMonthNumForReceipt=[];
+    this.InvoiceInGstForReceipt=[];
+    let MonthStartDate= this.datepipe.transform(this.addMonths(new Date(), -6), 'dd/MM/yyyy');
+    let MonthEndDate= this.datepipe.transform(new Date(), 'dd/MM/yyyy');
+    this._mainAPiServiceService.getSetData({ Dashboard: type,StartDate:MonthStartDate,EndDate:MonthEndDate }, 'GetDashboard').subscribe(res => {
+        console.log(res);
+        if (res.CODE == 200 && res.STATUS == "success") {
+            this.TotalInvoice=res.DATA.DASHBOARDTOTALS.INCGST;
+            res.DATA.DASHBOARDDATA.forEach(element => {
+                element.DATEDESC
+                let MonthWiseDate = element.DATEDESC.split("/");
+                var monthNum = MonthWiseDate[1];   // assuming Jan = 1
+                var shortName = moment.monthsShort(monthNum - 1); 
+                this.getMonthNumForReceipt.push(shortName);
+                this.InvoiceInGstForReceipt.push(element.INCGST);
+                
+            });
+            this.widget13= {
+                chartType:"bar",
+                colors:[{backgroundColor: "#43a047",
+                borderColor: "#42a5f5"}],
+                conversion:{
+                    value:res.DATA.DASHBOARDTOTALS.INCGST
+                },
+                datasets:[
+                    {data:this.InvoiceInGstForReceipt,
+                    label:'Total Receipt'}
+                ],
+                labels:this.getMonthNumForReceipt,
+                options:{
+                    layout:{
+                        padding:{
+                            bottom: 16,
+                            left: 16,
+                            right: 16,
+                            top: 24
+                        }
+                    },
+                    legend:{display:false},
+                    maintainAspectRatio:false,
+
+                },
+                scales:{
+                    xAxes:[{display:true}],
+                    yAxes:[{display:true ,ticks:{
+                        max:100000000,
+                        min:0,
+                        stepSize:20000000
+                    }},
+                    ]
+                }
+            
+            }
+           
+        }
+    });
+}
+ComparisionChart(){
+ this.widget5="empty";
+    this.widget5= {
+        chartType: 'line',
+        datasets : {
+            'today'    : [
+          
+                {
+                    label: 'Invoices',
+                    data : this.InvoiceInGst,
+                    fill : 'start'
+                },
+                {
+                    label: 'Expenses',
+                    data : this.InvoiceInGstForExpense,
+                    fill : 'start'
+
+                },
+               
+            ]
+        },
+        labels   : this.getMonthNumForExpense,
+        colors   : [
+            {
+                borderColor              : '#3949ab',
+                backgroundColor          : '#3949ab',
+                pointBackgroundColor     : '#3949ab',
+                pointHoverBackgroundColor: '#3949ab',
+                pointBorderColor         : '#ffffff',
+                pointHoverBorderColor    : '#ffffff'
+            },
+            {
+                borderColor              : 'rgba(30, 136, 229, 0.87)',
+                backgroundColor          : 'rgba(30, 136, 229, 0.87)',
+                pointBackgroundColor     : 'rgba(30, 136, 229, 0.87)',
+                pointHoverBackgroundColor: 'rgba(30, 136, 229, 0.87)',
+                pointBorderColor         : '#ffffff',
+                pointHoverBorderColor    : '#ffffff'
+            }
+        ],
+        options  : {
+            spanGaps           : false,
+            legend             : {
+                display: false
+            },
+            maintainAspectRatio: false,
+            tooltips           : {
+                position : 'nearest',
+                mode     : 'index',
+                intersect: false
+            },
+            layout             : {
+                padding: {
+                    left : 24,
+                    right: 32
+                }
+            },
+            elements           : {
+                point: {
+                    radius          : 4,
+                    borderWidth     : 2,
+                    hoverRadius     : 4,
+                    hoverBorderWidth: 2
+                }
+            },
+            scales             : {
+                xAxes: [
+                    {
+                        gridLines: {
+                            display: false
+                        },
+                        ticks    : {
+                            fontColor: 'rgba(0,0,0,0.54)'
+                        }
+                    }
+                ],
+                yAxes: [
+                    {display:true ,
+                        gridLines: {
+                            tickMarkLength: 16
+                        },
+                        ticks    : {
+                            max:100000000,
+                            min:0,
+                            stepSize: 20000000
+                        }
+                    }
+                ]
+            },
+            plugins            : {
+                filler: {
+                    propagate: false
+                }
+            }
+        }
+    }
+
+
+}
+
+
+
     /**
      * Register a custom plugin
      */
--- src/app/main/pages/main-dashboard/dashboard/dashboard.service.ts
+++ src/app/main/pages/main-dashboard/dashboard/dashboard.service.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs';
 import { MainAPiServiceService, BehaviorService } from 'app/_services';
 import { AnalyticsDashboardDb } from './dashboard-analytics';
 import { DatePipe } from '@angular/common';
+import * as moment from 'moment';
 
 @Injectable()
 export class DashboardService implements Resolve<any>
@@ -18,6 +19,9 @@ export class DashboardService implements Resolve<any>
     deviceValForUnbilled: any = [];
     UnbilledTotal: any;
     AgedDebatorTotal: any;
+    getMonthNum:any=[];
+    InvoiceInGst:any=[];
+    TotalInvoice: any;
 
     createDb(): any {
         return {
@@ -75,49 +79,95 @@ addMonths(date, months) {
      * @returns {Promise<any>}
      */
     getWidgets(): Promise<any> {
-       console.log(this.addMonths(new Date(), -6));
        let MonthStartDate= this.datepipe.transform(this.addMonths(new Date(), -6), 'dd/MM/yyyy');
        let MonthEndDate= this.datepipe.transform(new Date(), 'dd/MM/yyyy');
         return new Promise((resolve, reject) => {
-            this.AgedDebtorsArray = [];
-            this.deviceValForAged = [];
-            this.deviceValForUnbilled = [];
-            this._mainAPiServiceService.getSetData({ Dashboard: 'aged debtors' }, 'GetDashboard').subscribe(res => {
+            // this.AgedDebtorsArray = [];
+            // this.deviceValForAged = [];
+            // this.deviceValForUnbilled = [];
+            // this._mainAPiServiceService.getSetData({ Dashboard: 'aged debtors' }, 'GetDashboard').subscribe(res => {
             
-                if (res.CODE == 200 && res.STATUS == "success") {
-                    this.AgedDebtorsArray = res.DATA.DASHBOARDDATA;
-                    res.DATA.DASHBOARDDATA.forEach(element => {
-                        this.deviceValForAged.push({ name: element.DATEDESC, value: element.INCGST })
-                    });
-                    AnalyticsDashboardDb.widgets.widget7.devices = this.deviceValForAged
-                    this.AgedDebatorTotal = (res.DATA.DASHBOARDTOTALS.INCGST).toFixed(2);
+            //     if (res.CODE == 200 && res.STATUS == "success") {
+            //         this.AgedDebtorsArray = res.DATA.DASHBOARDDATA;
+            //         res.DATA.DASHBOARDDATA.forEach(element => {
+            //             this.deviceValForAged.push({ name: element.DATEDESC, value: element.INCGST })
+            //         });
+            //         AnalyticsDashboardDb.widgets.widget7.devices = this.deviceValForAged
+            //         this.AgedDebatorTotal = (res.DATA.DASHBOARDTOTALS.INCGST).toFixed(2);
               
         
-                }
+            //     }
 
-            }, reject);
-            this._mainAPiServiceService.getSetData({ Dashboard: 'unbilled WIP' }, 'GetDashboard').subscribe(res => {
+            // }, reject);
+            // this._mainAPiServiceService.getSetData({ Dashboard: 'unbilled WIP' }, 'GetDashboard').subscribe(res => {
     
-                this.itemVal = [];
-                this.AmountUnBilled = [];
-                if (res.CODE == 200 && res.STATUS == "success") {
-                    this.UnbilledWIPArray = res.DATA.DASHBOARDDATA;
-                    res.DATA.DASHBOARDDATA.forEach(element => {
-                        this.deviceValForUnbilled.push({ name: element.DATEDESC, value: element.INCGST })
-                    });
-                    this.UnbilledTotal = (res.DATA.DASHBOARDTOTALS.INCGST).toFixed(2);
-                    // this.behaviorService.totalDashboard({UnbilledTotal:(res.DASHBOARDTOTALS.EXGST).toFixed(2)});
-                    AnalyticsDashboardDb.widgets.widget2.devices = this.deviceValForUnbilled;
-                    this.widgets = AnalyticsDashboardDb.widgets;
-                    resolve(this.widgets);
+            //     this.itemVal = [];
+            //     this.AmountUnBilled = [];
+            //     if (res.CODE == 200 && res.STATUS == "success") {
+            //         this.UnbilledWIPArray = res.DATA.DASHBOARDDATA;
+            //         res.DATA.DASHBOARDDATA.forEach(element => {
+            //             this.deviceValForUnbilled.push({ name: element.DATEDESC, value: element.INCGST })
+            //         });
+            //         this.UnbilledTotal = (res.DATA.DASHBOARDTOTALS.INCGST).toFixed(2);
+            //         // this.behaviorService.totalDashboard({UnbilledTotal:(res.DASHBOARDTOTALS.EXGST).toFixed(2)});
+            //         AnalyticsDashboardDb.widgets.widget2.devices = this.deviceValForUnbilled;
+            //         this.widgets = AnalyticsDashboardDb.widgets;
+            //         resolve(this.widgets);
                
 
-                }
-            }, reject);
+            //     }
+            // }, reject);
             // this._mainAPiServiceService.getSetData({ Dashboard: 'invoices',StartDate:MonthStartDate,EndDate:MonthEndDate }, 'GetDashboard').subscribe(res => {
             //     console.log(res);
             //     if (res.CODE == 200 && res.STATUS == "success") {
+            //         this.TotalInvoice=res.DATA.DASHBOARDTOTALS.INCGST;
+            //         res.DATA.DASHBOARDDATA.forEach(element => {
+            //             element.DATEDESC
+            //             let MonthWiseDate = element.DATEDESC.split("/");
+            //             var monthNum = MonthWiseDate[1];   // assuming Jan = 1
+            //             var shortName = moment.monthsShort(monthNum - 1); 
+            //             this.getMonthNum.push(shortName);
+            //             this.InvoiceInGst.push(element.INCGST);
+                        
+            //         });
+            //         let widget11= {
+            //             chartType:"bar",
+            //             colors:[{backgroundColor: "#ff0000",
+            //             borderColor: "#42a5f5"}],
+            //             conversion:{
+            //                 value:res.DATA.DASHBOARDTOTALS.INCGST
+            //             },
+            //             datasets:[
+            //                 this.InvoiceInGst,
+            //                 {label:'TotalInvoiced'}
+            //             ],
+            //             labels:this.getMonthNum,
+            //             options:{
+            //                 layout:{
+            //                     padding:{
+            //                         bottom: 16,
+            //                         left: 16,
+            //                         right: 16,
+            //                         top: 24
+            //                     }
+            //                 },
+            //                 legend:{display:false},
+            //                 maintainAspectRatio:false,
 
+            //             },
+            //             scales:{
+            //                 xAxes:[{display:true}],
+            //                 yAxes:[{display:true ,ticks:{
+            //                     max:500,
+            //                     min:100
+            //                 }},
+            //                 ]
+            //             }
+                    
+            //         }
+            //         console.log(widget11);
+            //         this.widgets
+            //         console.log(AnalyticsDashboardDb.widgets);    
             //     }
             // }, reject);
             // this._mainAPiServiceService.getSetData({ Dashboard: 'expenses',StartDate:MonthStartDate,EndDate: MonthEndDate }, 'GetDashboard').subscribe(res => {
@@ -132,7 +182,6 @@ addMonths(date, months) {
 
             //     }
             // }, reject);
-
 
         });
     }

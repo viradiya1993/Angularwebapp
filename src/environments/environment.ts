f11ccfc6ede37edd64c5b1ec92fcf7f79835de54
kalpeshmoon <kalpesh@moontechnolabs.com>
2019-11-28 16:11:03 +0530

    no message

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
     APIEndpoint: 'https://apitest.silq.com.au/',
     ReportUrl: 'https://apitest.silq.com.au/',
     APIKEY: 'SNGMTUEEB2AJBFC9',
-    VERSION: '1.6.7',
+    VERSION: '1.6.8',
     WORDLIMIT: 40,
 };
 

355ea6447b80c1285f7c3d64696c59003b2b8fcf
kalpeshmoon <kalpesh@moontechnolabs.com>
2019-12-12 17:48:06 +0530

    fixed calcutions on recalculations

--- src/app/main/pages/time-billing/re-calc-timeEntrie-dialoge/re-calc-timeEntrie-dialoge.component.ts
+++ src/app/main/pages/time-billing/re-calc-timeEntrie-dialoge/re-calc-timeEntrie-dialoge.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 import { fuseAnimations } from '@fuse/animations';
 import { FormGroup } from '@angular/forms';
 import { MainAPiServiceService, BehaviorService, TimersService } from 'app/_services';
-import { MatDialogRef } from '@angular/material';
+import { MatDialogRef, MatDialog } from '@angular/material';
 import { ToastrService } from 'ngx-toastr';
-
+import { FuseConfirmDialogComponent } from '@fuse/components/confirm-dialog/confirm-dialog.component';
 @Component({
   selector: 'app-re-calc-timeEntrie-dialoge',
   templateUrl: './re-calc-timeEntrie-dialoge.component.html',
@@ -13,22 +13,26 @@ import { ToastrService } from 'ngx-toastr';
   encapsulation: ViewEncapsulation.None
 })
 export class ReCalcTimeEntriesDialogeComponent implements OnInit {
+  confirmDialogRef: MatDialogRef<FuseConfirmDialogComponent>;
   @Input() SettingForm: FormGroup;
   @Input() errorWarningData: any;
+
   addData: any = [];
   TemplateName: any;
   getSelectedWIP: any;
-  getSelectedWIPNewPrice: any;
+  getSelectedWIPshow: any;
+  getSelectedWIPNewPrice: any = [];
   IsWipData: boolean = false;
+  isLoadingResults: boolean = true;
   isspiner: boolean = false;
   successMsg: any = 'Time entry update successfully';
-  constructor(private _mainAPiServiceService: MainAPiServiceService, private Timersservice: TimersService, private toastr: ToastrService,
+  constructor(private _mainAPiServiceService: MainAPiServiceService, private Timersservice: TimersService, private toastr: ToastrService, public MatDialog: MatDialog,
     public dialogRef: MatDialogRef<ReCalcTimeEntriesDialogeComponent>, private behaviourService: BehaviorService) {
 
     this.behaviourService.workInProgress$.subscribe(result => {
       if (result) {
-        console.log(result);
         this.getSelectedWIP = result;
+        this.getSelectedWIPshow = result;
       }
     });
   }
@@ -44,23 +48,86 @@ export class ReCalcTimeEntriesDialogeComponent implements OnInit {
         QUANTITYTYPE: this.getSelectedWIP.QUANTITYTYPE
       }
       this._mainAPiServiceService.getSetData(sendData, 'CalcWorkItemCharge').subscribe(response => {
-        console.log(response);
         if (response.CODE == 200 && response.STATUS == "success") {
           this.getSelectedWIPNewPrice = response.DATA;
-          // this.getSelectedWIP = this.getSelectedWIPNewPrice;
+          if (this.getSelectedWIP.PRICE == this.getSelectedWIPNewPrice.PRICE)
+            this.IsWipData = false;
         }
+        this.isLoadingResults = false;
       })
     } else {
+      this.isLoadingResults = false;
       this.IsWipData = false;
     }
   }
 
 
-  save() {
+  ChangePrice() {
     this.isspiner = true;
-    let PostData: any = this.getSelectedWIP;
+    this.getSelectedWIPshow.PRICE = this.getSelectedWIPNewPrice.PRICE;
+    this.getSelectedWIPshow.GST = this.getSelectedWIPNewPrice.GST;
+    this.getSelectedWIPshow.PRICEINCGST = this.getSelectedWIPNewPrice.PRICEINCGST;
+    let PostData: any = this.getSelectedWIPshow;
     this.successMsg = 'Time entry Update successfully';
     let PostTimeEntryData: any = { FormAction: 'update', VALIDATEONLY: true, Data: PostData };
+    this.Timersservice.SetWorkItems(PostTimeEntryData).subscribe(res => {
+      if (res.CODE == 200 && res.STATUS == "success") {
+        this.checkValidation(res.DATA.VALIDATIONS, PostTimeEntryData);
+      } else if (res.CODE == 451 && res.STATUS == 'warning') {
+        this.checkValidation(res.DATA.VALIDATIONS, PostTimeEntryData);
+      } else if (res.CODE == 450 && res.STATUS == 'error') {
+        this.checkValidation(res.DATA.VALIDATIONS, PostTimeEntryData);
+      } else if (res.MESSAGE == 'Not logged in') {
+        this.dialogRef.close(false);
+      }
+      this.isspiner = false;
+    }, err => {
+      this.isspiner = false;
+      this.toastr.error(err);
+    });
+  }
+
+  checkValidation(bodyData: any, PostTimeEntryData: any) {
+    let errorData: any = [];
+    let warningData: any = [];
+    let tempError: any = [];
+    let tempWarning: any = [];
+    // errorData
+    bodyData.forEach(function (value) {
+      if (value.VALUEVALID == 'NO') {
+        errorData.push(value.ERRORDESCRIPTION);
+        tempError[value.FIELDNAME] = value;
+      } else if (value.VALUEVALID == 'WARNING') {
+        tempWarning[value.FIELDNAME] = value;
+        warningData.push(value.ERRORDESCRIPTION);
+      }
+    });
+    this.errorWarningData = { "Error": tempError, 'warning': tempWarning };
+    if (Object.keys(errorData).length != 0) {
+      this.toastr.error(errorData);
+      this.isspiner = false;
+    } else if (Object.keys(warningData).length != 0) {
+      // this.toastr.warning(warningData);
+      this.confirmDialogRef = this.MatDialog.open(FuseConfirmDialogComponent, {
+        disableClose: true,
+        width: '100%',
+        data: warningData
+      });
+      this.confirmDialogRef.componentInstance.confirmMessage = 'Are you sure you want to Save?';
+      this.confirmDialogRef.afterClosed().subscribe(result => {
+        if (result) {
+          this.isspiner = true;
+          this.saveTimeEntry(PostTimeEntryData);
+        }
+        this.confirmDialogRef = null;
+      });
+    } else if (Object.keys(warningData).length == 0 && Object.keys(errorData).length == 0) {
+      this.saveTimeEntry(PostTimeEntryData);
+      this.isspiner = false;
+    }
+  }
+  saveTimeEntry(PostTimeEntryData: any) {
+    PostTimeEntryData.VALIDATEONLY = false;
     this.Timersservice.SetWorkItems(PostTimeEntryData).subscribe(res => {
       if (res.CODE == 200 && res.STATUS == "success") {
         this.toastr.success(this.successMsg);
@@ -78,6 +145,5 @@ export class ReCalcTimeEntriesDialogeComponent implements OnInit {
       this.toastr.error(err);
     });
   }
-
 
 }
